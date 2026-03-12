771fe27a2d3895961491af924880efa99dc96b71
HashimKhan923 <khanhash1994@gmail.com>
2026-03-13 04:27:50 +0500

    feferdwddwdswdwddwe

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1182,7 +1182,6 @@ footer {
     
     @if(!empty($settings['site_logo']))
       <img src="{{ asset($settings['site_logo']) }}" alt="{{ $settings['site_name'] ?? 'SK Artistic' }}" style="-webkit-text-fill-color:unset;background:none;height:36px;">
-    asset($settings['site_logo'])
     @else
     <div class="nav-logo-glyph"><span>SK</span></div>
       {{ $settings['site_name'] ?? 'SK ARTISTIC' }}
